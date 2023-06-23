f438db72be63613d7cabc37566274a32f5f3a0a8
RAHUL SAINI <93477046+rahulsaini-js@users.noreply.github.com>
2023-06-23 09:08:09 +0530

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,36 @@
+const employeesData = [
+    {
+      id: 1,
+      firstName: 'Rahul',
+      lastName: 'saini',
+      email: 'rahul@example.com',
+      number: '95000',
+      date: '2019-04-11'
+    },
+    {
+      id: 2,
+      firstName: 'shivam',
+      lastName: 'kumar',
+      email: 'shivam@example.com',
+      number: '80000',
+      date: '2019-04-17'
+    },
+    {
+      id: 3,
+      firstName: 'panda',
+      lastName: 'verma',
+      email: 'panda@example.com',
+      number: '79000',
+      date: '2019-05-01'
+    },
+    {
+      id: 4,
+      firstName: 'charul',
+      lastName: 'km',
+      email: 'km@example.com',
+      number: '56000',
+      date: '2019-05-03'
+    }
+  ];
+  
+  export { employeesData };
